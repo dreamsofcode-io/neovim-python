54d5865be02af4862fd79fa599c6674d757f5096
Sergey Pletnev <71025831+liltousin@users.noreply.github.com>
2023-12-09 17:57:14 +0300

    Update null-ls.lua
    Fix mypy "Cannot find implementation or library stub" error со сторонними библеотеками

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -4,7 +4,12 @@ local null_ls = require('null-ls')
 local opts = {
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.mypy,
+    null_ls.builtins.diagnostics.mypy.with({
+      extra_args = function()
+      local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+      return { "--python-executable", virtual .. "/bin/python3" }
+      end,
+    }),
     null_ls.builtins.diagnostics.ruff,
   },
   on_attach = function(client, bufnr)
